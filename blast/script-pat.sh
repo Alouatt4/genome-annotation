6a9094b16ceab39cdc7cf791061525a87a4dd24d
Patrick Souza <90911588+Alouatt4@users.noreply.github.com>
2025-02-26 16:29:33 -0300

    Update script-pat.sh

--- blast/script-pat.sh
+++ blast/script-pat.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+sequences=(
+	"mqua-yellow-e_yellow-g_yellow-g2.fasta"
+	"mqua-mrjp-like_yellow-e3_yellow-h.fasta"
+	)
+
+for seq in "${sequences[@]}";
+	do
+	nome_base = $(basename "$seq" .fasta)
+	outfile = "result-0-${nome_base}"
+	
+	blastn -db ../db/Scaptotrigona.bipunctata.LIB.2023.fa -query "$seq" -outfmt 0 -out "$outfile"
+	echo "Processing file: $seq -> $outfile"
+done
+
+echo "All files have been processed!"
+
+#end
